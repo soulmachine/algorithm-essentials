07c8a1850c7b8d9045289e2cc45e233dd32b847d
Frank Dai <soulmachine@gmail.com>
2016-03-28 01:22:25 -0700

    Synced from java to cpp

---
+++ cpp/code/best-time-to-buy-and-sell-stock-with-cooldown-1.java
@@ -0,0 +1,18 @@
+// Best Time to Buy and Sell Stock with Cooldown
+// Time Complexity: O(n), Space Complexity: O(n)
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
+
+        int[] sell = new int[prices.length];
+        int[] buy = new int[prices.length];
+        sell[0] = 0;
+        buy[0] = -prices[0];
+
+        for (int i = 1; i < prices.length; ++i) {
+            sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i]);
+            buy[i] = Math.max(buy[i - 1], (i > 1 ? sell[i - 2] : 0) - prices[i]);
+        }
+        return sell[prices.length - 1];
+    }
+}
