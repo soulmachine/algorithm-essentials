48724279bc173564b78f7e729c92e70675d991f3
Frank Dai <fangqin.dai@cmcm.com>
2017-04-17 15:19:35 -0700

    Refactored the code of regular-expression-matching

--- cpp/code/regular-expression-matching.java
+++ cpp/code/regular-expression-matching.java
@@ -1,8 +1,14 @@
 // Regular Expression Matching
-// 递归版，时间复杂度O(n)，空间复杂度O(1)
+// Time complexity: O(n)
+// Space complexity: O(1)
 class Solution {
     public boolean isMatch(final String s, final String p) {
         return isMatch(s, 0, p, 0);
+    }
+    private static boolean matchFirst(String s, int i, String p, int j) {
+        if (j == p.length()) return i == s.length();
+        if (i == s.length()) return j == p.length();
+        return p.charAt(j) == '.' || s.charAt(i) == p.charAt(j);
     }
     private static boolean isMatch(String s, int i, String p, int j) {
         if (j == p.length()) return i == s.length();
@@ -10,16 +16,13 @@ class Solution {
         // next char is not '*', then must match current character
         final char b = p.charAt(j);
         if (j == p.length() - 1 || p.charAt(j + 1) != '*') {
-            if (i < s.length() && (b == s.charAt(i) || b == '.'))
-                return isMatch(s, i + 1, p, j + 1);
+            if (matchFirst(s, i, p, j)) return isMatch(s, i + 1, p, j + 1);
             else return false;
         } else { // next char is '*'
-            while (i < s.length() && (b == s.charAt(i) || b == '.')) {
-                if (isMatch(s, i, p,  j + 2))
-                    return true;
-                i++;
-            }
-            return isMatch(s, i, p,  j + 2);
+            if (isMatch(s, i, p, j+2)) return true;  // try the length of 0
+            while (matchFirst(s, i, p, j))  // try all possible lengths 
+                if (isMatch(s, ++i, p, j+2)) return true;
+            return false;
         }
     }
 }
--- java/code/regular-expression-matching.cpp
+++ java/code/regular-expression-matching.cpp
@@ -1,27 +1,29 @@
 // Regular Expression Matching
-// 递归版，时间复杂度O(n)，空间复杂度O(1)
+// Time complexity: O(n)
+// Space complexity: O(1)
 class Solution {
 public:
     bool isMatch(const string& s, const string& p) {
         return isMatch(s.c_str(), p.c_str());
     }
 private:
+    bool matchFirst(const char *s, const char *p) {
+        return *p == *s || (*p == '.' && *s != '\0');
+    }
+
     bool isMatch(const char *s, const char *p) {
-        if (*p == '\0') return *s == '\0';
+        if (*p == '\0') return *s == '\0';    //empty
 
         // next char is not '*', then must match current character
         if (*(p + 1) != '*') {
-            if (*s != '\0' && (*p == *s || *p == '.'))
-                return isMatch(s + 1, p + 1);
-            else
-                return false;
+            if (matchFirst(s, p)) return isMatch(s + 1, p + 1);
+            else return false;
         } else { // next char is '*'
-            while (*s != '\0' && (*p == *s || *p == '.')) {
-                if (isMatch(s, p + 2))
-                    return true;
-                s++;
-            }
-            return isMatch(s, p + 2);
+            if (isMatch(s, p + 2)) return true;    // try the length of 0
+            while ( matchFirst(s, p) )       // try all possible lengths 
+                if (isMatch(++s, p + 2)) return true;
+            return false;
         }
     }
 };
+
