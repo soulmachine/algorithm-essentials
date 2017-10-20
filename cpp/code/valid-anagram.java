0f3dbc1ed55b59efaa8cea3e0c57d55c1e68b4db
soulmachine <soulmachine@gmail.com>
2017-10-20 00:32:51 -0700

    refactor

--- cpp/code/valid-anagram.java
+++ cpp/code/valid-anagram.java
@@ -2,17 +2,15 @@
 // Time Complexity: O(n), Space Complexity: O(1)
 public class Solution {
     public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
         final int[] map = new int[ALPHABET_SIZE];
 
         for (int i = 0; i < s.length(); ++i) {
             ++map[s.charAt(i) - 'a'];
-        }
-        for (int i = 0; i < t.length(); ++i) {
             --map[t.charAt(i) - 'a'];
         }
         for (int x : map) {
-            if (x < 0) return false;
-            if (x > 0) return false;
+            if (x != 0) return false;
         }
         return true;
     }
