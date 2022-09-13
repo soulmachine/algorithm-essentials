809ff0ce68f5dd39e213201d34cb0b5736e8e2cf
Frank Dai <soulmachine@gmail.com>
2022-09-13 05:17:59 -0700

    Created a brand new project and copied everything over

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,65 +1,61 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
 const math = require('remark-math');
 const katex = require('rehype-katex');
 
-module.exports = {
+/** @type {import('@docusaurus/types').Config} */
+const config = {
   title: "算法珠玑",
   tagline: "以少胜多",
   url: "https://soulmachine.github.io",
   baseUrl: "/",
-  onBrokenLinks: "warn",
-  favicon: "img/favicon.ico",
-  organizationName: "soulmachine", // Usually your GitHub org/user name.
-  projectName: "algorithm-essentials", // Usually your repo name.
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'soulmachine', // Usually your GitHub org/user name.
+  projectName: 'algorithm-essentials', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'zh-CN',
+    locales: ['en', 'zh-CN'],
+  },
+
   stylesheets: [
-    // from https://github.com/KaTeX/KaTeX#starter-template
     {
-      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
-      type: "text/css",
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
       integrity:
-        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
-      crossorigin: "anonymous",
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
     },
   ],
-  themeConfig: {
-    navbar: {
-      title: "算法珠玑",
-      logo: {
-        alt: "算法珠玑",
-        src: "img/logo.svg",
-      },
-      items: [
-        {
-          href: "https://github.com/soulmachine/algorithm-essentials",
-          label: "GitHub",
-          position: "right",
-        },
-      ],
-    },
-    algolia: {
-      appId: 'SJ9D0I9C67',
-      apiKey: "f611fdd8408b5a18f8cb862bdebf72fb",
-      indexName: "algorithm_essentials_soulmachine",
-      searchParameters: {}, // Optional (if provided by Algolia)
-    },
-    prism: {
-      theme: require("prism-react-renderer/themes/github"),
-    },
-  },
+
   presets: [
     [
-      "@docusaurus/preset-classic",
-      {
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
         docs: {
-          routeBasePath: "/", // docs-only mode
-          sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
+          routeBasePath: "/", // docs-only mode, see https://docusaurus.io/docs/docs-introduction#docs-only-mode
+          sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
             "https://github.com/soulmachine/algorithm-essentials/edit/master/",
           remarkPlugins: [math],
           rehypePlugins: [[katex, { strict: false }]],
         },
+        blog: false, // disable the blog plugin
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
         gtag: {
           trackingID: "UA-7583537-12",
@@ -67,7 +63,49 @@ module.exports = {
         googleAnalytics: {
           trackingID: "UA-7583537-12",
         },
-      },
+      }),
     ],
   ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      docs: {
+        sidebar: {
+          hideable: true,
+          autoCollapseCategories: true,
+        },
+      },
+      colorMode: {
+        defaultMode: 'light',
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
+      },
+      navbar: {
+        title: "算法珠玑",
+        logo: {
+          alt: "算法珠玑",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            href: "https://github.com/soulmachine/algorithm-essentials",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      algolia: {
+        appId: 'IMD1FFERB9',
+        apiKey: "63b2c582ff61a2e223b8fc135e3bc448",
+        indexName: "index_algorithm_essentials",
+      },
+      prism: {
+        additionalLanguages: ['java'],
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
 };
+
+module.exports = config;
